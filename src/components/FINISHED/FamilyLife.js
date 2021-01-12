574f313d3a06bd1c48949e3b330ff226c639a8e1
Jaakko-cyber <64479773+Jaakko-cyber@users.noreply.github.com>
2021-01-12 16:17:18 +0100

    Update FamilyLife.js

--- src/components/FINISHED/FamilyLife.js
+++ src/components/FINISHED/FamilyLife.js
@@ -17,7 +17,7 @@ const EndofFAMILY = ({ radarData, radarOptions, value }) => {
     <>
       <Jello duration="1.0s" delay="0.2s">
         {/* Pass img name to container as answer.imgName */}
-        <FContainerS>
+        <FContainerS imgName={answer.imgName}>
           <FContent>
             <Jello duration="1.0s" delay="0.4s">
               <FItems>
